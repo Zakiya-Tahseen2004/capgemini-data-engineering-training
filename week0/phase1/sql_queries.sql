12f65fcf54b4f17d5e8e7fc44d06d14860f78ad7
Zakiya-Tahseen2004 <zakiyaakvd@gmail.com>
2026-03-28 17:37:05 +0530

    Add SQL queries for customers table creation and data
    Create customers table and insert sample data with queries.

---
+++ week0/phase1/sql_queries.sql
@@ -0,0 +1,23 @@
+CREATE TABLE customers (
+    customer_id INT,
+    customer_name VARCHAR(50),
+    city VARCHAR(50),
+    age INT
+);
+INSERT INTO customers VALUES
+(1, 'Ravi', 'Hyderabad', 25),
+(2, 'Sita', 'Chennai', 32),
+(3, 'Arun', 'Hyderabad', 28);
+SELECT * FROM customers;
+
+SELECT * FROM customers
+WHERE city = 'Chennai';
+
+SELECT * FROM customers
+WHERE age > 25;
+
+SELECT customer_name, city FROM customers;
+
+SELECT city, COUNT(*) AS total_customers
+FROM customers
+GROUP BY city;
